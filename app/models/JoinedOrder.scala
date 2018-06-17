199563da530370c1727deed2395d8dfaf05ee99d
Andrei Kiryieuski <andrey.kirievskiy.94@gmail.com>
2018-06-17 23:11:56 +0300

    task4, com1

---
+++ app/models/JoinedOrder.scala
@@ -0,0 +1,9 @@
+package models
+
+import play.api.libs.json._
+
+case class JoinedBasketProduct(basket_id:Int, user_id: Int,  product_id: Int, basket2_id: Int, id: Int, name: String, description: String, price: Int)
+
+object JoinedBasketProduct {
+  implicit val JoinedBasketProductFormat = Json.format[JoinedBasketProduct]
+}
---
+++ app/models/JoinedPay.scala
@@ -0,0 +1,4 @@
+package models
+
+case class JoinedOrder(id: Int, basket_id: Int, total: Int)
+
